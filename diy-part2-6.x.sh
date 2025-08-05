9ce618eda023b62fa8186b5404345fc050a08f55
lavey404 <laveyluo@gmail.com>
2025-08-05 11:49:35 +0800

    Update diy-part2-6.x.sh

--- diy-part2-6.x.sh
+++ diy-part2-6.x.sh
@@ -30,3 +30,26 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-eqosplus package/luci-a
 rm -rf package/luci-app-amlogic
 git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
+# Git稀疏克隆，只克隆指定目录到本地
+function git_sparse_clone() {
+  branch="$1" repourl="$2" && shift 2
+  git clone --depth=1 -b $branch --single-branch --filter=blob:none --sparse $repourl
+  repodir=$(echo $repourl | awk -F '/' '{print $(NF)}')
+  cd $repodir && git sparse-checkout set $@
+  mv -f $@ ../package
+  cd .. && rm -rf $repodir
+}
+
+# 更新 golang 1.24 版本
+rm -rf feeds/packages/lang/golang
+git clone https://github.com/sbwml/packages_lang_golang -b 24.x feeds/packages/lang/golang
+
+# 移除要替换的包
+rm -rf feeds/packages/net/{xray-core,v2ray-core,v2ray-geodata,sing-box}
+
+# 科学上网插件
+git clone https://github.com/xiaorouji/openwrt-passwall-packages package/passwall-packages
+git clone https://github.com/xiaorouji/openwrt-passwall package/passwall
+git clone https://github.com/sbwml/luci-app-mosdns -b v5 package/mosdns
+
+
